3180260ca9abca4ec18fdb48d96f59865b2d9d8e
Wesley Hill <hakobyte@gmail.com>
2012-08-10 12:45:25 +0100

    Added index file to start simulator.

---
+++ index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html>
+
+<head>
+
+<meta charset="ISO-8859-1">
+
+<title>Index page</title>
+
+<meta name="google-translate-customization" content="63fbf869928a7f97-d29e34bc776152e4-g21506ed855530fb0-2b"></meta>
+
+<link rel="icon" href="images/GDOS.gif" type="image/gif" >
+
+<link href="fonts.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+
+@import url(http://fonts.googleapis.com/css?family=Averia+Sans+Libre);
+
+@import url(http://fonts.googleapis.com/css?family=Press+Start+2P);
+
+p{
+	font-family: "Averia Sans Libre", cursive;
+	text-align: center;
+}
+
+
+h1
+{
+font-family: 'Conv_Catull';
+}
+
+
+.Googol_Colour {
+	color: #999;
+}
+.Googol_Colour_0 {
+	color: #666;
+}
+.Googol_Colour_1 {
+	color: #CCC;
+	font-family: Conv_Catull;
+}
+.Googol_Colour_2 {
+	color: #999;
+}
+.Googol_Colour_3 {
+	color: #666;
+}
+.Googol_Colour_4 {
+	color: #333;
+}
+.Googol_Colour_5 {
+	color: #CCC;
+}
+.Googol_Colour_6 {
+	color: #333;
+}
+.Googol_Colour_7 {
+	color: #666;
+}
+.Googol_Colour_8 {
+	color: #999;
+}
+.Googol_Colour_9 {
+	color: #666;
+}
+.Googol_Colour_10 {
+	color: #999;
+}
+
+.Googol_Simulator_Font_Colour {
+	font-family: 'Averia Sans Libre', cursive;
+	color:#333
+	font-size: 38px;
+}
+
+
+.QuickTime {
+	color: #0083C4;
+}
+
+.minified {
+	color: #96C;
+}
+
+.RPi {
+	color: #FF3333;
+	font-weight: bold;
+}
+
+.ienvyu {
+	color: #003366;
+}
+
+.Hako_Colour {
+	color: #F60;
+}
+a:hover {
+	color: #FF802C;
+	text-decoration: underline;
+}
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+h1,h2,h3,h4,h5,h6 {
+	font-family: "Conv_Catull";
+	font-size: 76px;
+}
+#apDiv1 {
+	position: absolute;
+	width: 266px;
+	height: 51px;
+	z-index: 1;
+	left: 683px;
+	top: 827px;
+}
+
+body {
+	background-color: #FFF;
+}
+</style>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+ <embed src="Sound/glados_s6.m4a" id="gladoss6" hidden="True" autostart="False" loop="False" cache="true">  </embed>
+  <embed src="Sound/glados_s7.m4a" id="gladoss6" hidden="True" autostart="False" loop="False" cache="true">  </embed>
+  <embed src="Sound/glados_s8.m4a" id="gladoss6" hidden="True" autostart="False" loop="False" cache="true">  </embed>
+  <embed src="Sound/glados_s9.m4a" id="gladoss6" hidden="True" autostart="False" loop="False" cache="true">  </embed>
+  <embed src="Sound/glados_s10.m4a" id="gladoss6" hidden="True" autostart="False" loop="False" cache="true">
+</embed>
+
+
+
+<!--[if IE]>
+<script type="text/javascript">
+window.location = "IE9.html";
+</script>
+<![endif]-->
+
+</head>
+
+<body>
+
+
+<p>
+ 
+</p>
+<p>&nbsp;</p>
+
+<h1 align="center"><span class="Googol_Colour">G</span><span class="Googol_Colour_0">o</span><span class="Googol_Colour_1">o</span><span class="Googol_Colour_2">g</span><span class="Googol_Colour_3">o</span><span class="Googol_Colour_4">l</span> <span class="Googol_Colour_5">G</span><span class="Googol_Colour_6">L</span><span class="Googol_Colour_7">a</span><span class="Googol_Colour_8">D</span><span class="Googol_Colour_9">O</span><span class="Googol_Colour_10">S</span><span class="Googol_Simulator_Font_Colour">:simulator</span><span class="Googol_Colour"><font size="-1" class="Googol_Simulator_Font_Colour"> <sup> (source code)</sup></font></span></h1>
+
+<p align="center">
+<p align="center">This is the source code version of the Googol GLaDOS:simulator!
+<p align="center">You are also looking at an example page index of the simulator so you can get started making your own if you want.
+<p align="center">Please read the '<a href="README.md">README.md</a>' for further information.
+<p align="center">
+<p align="center">---</p>
+<p align="center">If you wish to contribute to the project in making it better, please fork or download the files from the repo!</p>
+<p align="center"><a href="https://github.com/hak0/googol-glados-simulator">https://github.com/hak0/googol-glados-simulator</a></p>
+<p align="center"><font size="1">=[],</font></p>
+<p align="center" class="RPi">v.1.0</p>
+<p align="center">---</p>
+<p align="center"><br>
+  &nbsp;
+  <input value="Launch simulator" onClick="window.location='step1.php'" type="button">
+</p>
+<p>&nbsp;</p>
+<div id="google_translate_element"></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true, gaTrack: true, gaId: 'UA-25782369-1'}, 'google_translate_element');
+}
+  </script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+</body>
+
+</html>
